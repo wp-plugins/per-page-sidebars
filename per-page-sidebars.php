8967241d91a0b7144240825bfd7c23f6bb37b0dd
BrianLayman <BrianLayman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-21 00:07:38 +0000

    Version 2.0 initial check in.
    git-svn-id: https://plugins.svn.wordpress.org/per-page-sidebars/trunk@561412 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- per-page-sidebars.php
+++ per-page-sidebars.php
@@ -3,11 +3,11 @@
 Plugin Name: Per Page Sidebars
 Plugin URI: http://TheCodeCave.com/plugins/per-page-sidebars
 Description: Allows the creation and display of custom sidebars for each page on your site.
-Version: 1.0
+Version: 2.0.0
 Author: Brian Layman
 Author URI: http://eHermitsInc.com
 License: GPLv2
-Requires: 2.5
+Requires: 3.1.0
 Original Design Notes: 
 	In an Edit Page screen display a radio of all registered sidebars, save the choice it to postmeta. 
 	When a page is loaded/displayed, if the postmeta value exists overwrite the replaced sidebar's 
@@ -18,7 +18,7 @@ Original Design Notes:
 	If you can't change widgets you shouldn't be able to create a custom sidebar.
 */
 
-/*	Copyright 2010 Brian Layman (email : brian@ehermitsinc.com)
+/*	Copyright 2012 Brian Layman ( email : plugins@ehermitsinc.com )
 
 		This program is free software; you can redistribute it and/or modify
 		it under the terms of the GNU General Public License, version 2, as 
@@ -37,8 +37,8 @@ Original Design Notes:
 // ************************************************************
 // Constants
 // ************************************************************
-define('PPS_KEY_REPLACED', '_tcc-pps-replace');
-define('SIDEBAR_PREFIX', 'PPS-');
+define( 'PPS_KEY_REPLACED', '_tcc-pps-replace' );
+define( 'SIDEBAR_PREFIX', 'PPS-' );
 
 // ************************************************************
 // Helper Functions
@@ -49,7 +49,7 @@ define('SIDEBAR_PREFIX', 'PPS-');
 // ************************************************************
 function get_pps_post_ids() {
     global $wpdb;
-    $posts_w_sidebars = $wpdb->get_col( $wpdb->prepare("SELECT post_id FROM $wpdb->postmeta WHERE meta_key = %s", PPS_KEY_REPLACED) );
+    $posts_w_sidebars = $wpdb->get_col( $wpdb->prepare( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = %s", PPS_KEY_REPLACED ) );
     return $posts_w_sidebars;
 }
 
@@ -58,8 +58,8 @@ function get_pps_post_ids() {
 //
 //  Used to mark a selection as checked in HTML.
 // ************************************************************
-function tcc_pps_add_checked($testvalue, $value) {
-	if ($testvalue == $value) 
+function tcc_pps_add_checked( $testvalue, $value ) {
+	if ( $testvalue == $value ) 
  		return ' checked="checked" ';
 }
 
@@ -75,8 +75,10 @@ function tcc_pps_add_checked($testvalue, $value) {
 // ************************************************************
 function pps_add_custom_box() {
 	// This security check is also verified upon save.
-	if ( current_user_can('edit_theme_options') )
-		add_meta_box('pps_sectionid', __( 'Custom Sidebar', 'tcc_pps_domain' ), 'pps_inner_custom_box', 'page', 'advanced', 'high' );
+	if ( current_user_can( 'edit_theme_options' ) ) {
+		add_meta_box( 'pps_sectionid', __( 'Custom Sidebar', 'tcc_pps_domain' ), 'pps_inner_custom_box', 'post', 'advanced', 'high' );
+		add_meta_box( 'pps_sectionid', __( 'Custom Sidebar', 'tcc_pps_domain' ), 'pps_inner_custom_box', 'page', 'advanced', 'high' );
+	}
 }
 
 // ************************************************************
@@ -84,30 +86,30 @@ function pps_add_custom_box() {
 //
 //  This displays the sidebar replacement selection box
 // ************************************************************
-function pps_inner_custom_box($post) {
+function pps_inner_custom_box( $post ) {
 	global $wp_registered_sidebars;
-	$replaced_sidebar=get_post_meta($post->ID, PPS_KEY_REPLACED, true);
-	$pps_active_on_post = (isset($wp_registered_sidebars[$replaced_sidebar]) );
+	$replaced_sidebar=get_post_meta( $post->ID, PPS_KEY_REPLACED, true );
+	$pps_active_on_post = ( isset( $wp_registered_sidebars[ $replaced_sidebar ] ) );
 
-	echo '<input type="hidden" id="pps_nonce" name="pps_nonce" value="' . wp_create_nonce(plugin_basename(__FILE__) ) . '" />';
+	echo '<input type="hidden" id="pps_nonce" name="pps_nonce" value="' . wp_create_nonce( plugin_basename( __FILE__ ) ) . '" />';
 	echo '<input type="checkbox" value="1" id="tcc-pps-active" name="tcc-pps-active" class="tcc-pps-checkbox" ' .
-		tcc_pps_add_checked($pps_active_on_post, true) . '/> <label for="tcc-pps-active">' . __('Activate Custom Sidebar?', 'tcc_pps_domain' ) . 
+		tcc_pps_add_checked( $pps_active_on_post, true ) . '/> <label for="tcc-pps-active">' . __( 'Activate Custom Sidebar?', 'tcc_pps_domain' ) . 
 		'</label><br/><br/>';
 	echo '<div class="tcc-pps-radio-replace-buttons">';
-	echo '	<label for="tcc-pps-replace">' . __('Select a sidebar to replace:', 'tcc_pps_domain' ) . '</label><br/>';
+	echo '	<label for="tcc-pps-replace">' . __( 'Select a sidebar to replace:', 'tcc_pps_domain' ) . '</label><br/>';
 	// Print the list of sidebars in the drop down
-	foreach ($wp_registered_sidebars as $id => $sidebar) {
-		$description = $sidebar['description'];
-		if ( isset($sidebar['name']) ) {
-			$name = $sidebar['name'];
+	foreach ( $wp_registered_sidebars as $id => $sidebar ) {
+		$description = $sidebar[ 'description' ];
+		if ( isset( $sidebar[ 'name' ] ) ) {
+			$name = $sidebar[ 'name' ];
 		} else {
 			$name = $id;
 		}
 
 		// Eliminate any self created sidebars or this could get messy.
-		if (substr($name, 0, strlen(SIDEBAR_PREFIX) ) != SIDEBAR_PREFIX) {
+		if ( substr( $name, 0, strlen( SIDEBAR_PREFIX ) ) != SIDEBAR_PREFIX ) {
 			echo '			<span class="tcc-pps-radio-option"><input type="radio" name="tcc-pps-replace" value="' . $id . 
-			'" class="tcc-pps-radio" ' . tcc_pps_add_checked($replaced_sidebar, $id) . '/> ' . $name . ' - ' . $description . '</span><br/>';
+			'" class="tcc-pps-radio" ' . tcc_pps_add_checked( $replaced_sidebar, $id ) . '/> ' . $name . ' - ' . $description . '</span><br/>';
 		}
 	}
 	echo '		</div>';
@@ -118,29 +120,32 @@ function pps_inner_custom_box($post) {
 //
 //  Recieves the sidebar replacement data when a page is saved.
 // ************************************************************
-function pps_save_postdata($post_id) {
+function pps_save_postdata( $post_id ) {
 	global $wp_registered_sidebars;
-	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return $post_id;
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return $post_id;
 
 	// This will eventualy need to be adjusted to deal 'edit_CUSTOMPOSTTYPE' permissions as the core does.
 	// Assuming, of course, I extend the plugin to handle more than pages.
-	if (('page' == $_POST['post_type'] ) and current_user_can('edit_theme_options')) {
-		if(!current_user_can('edit_page', $post_id)) return $post_id;
+	if ( ( current_user_can( 'edit_theme_options' ) ) && 
+			( ( ( 'page' == $_POST[ 'post_type' ] ) && current_user_can( 'edit_page', $post_id ) ) ||
+			  ( ( 'post' == $_POST[ 'post_type' ] ) && current_user_can( 'edit_post', $post_id ) ) ) ){
+		// Fall through to the rest of the function
 	} else {
+		// Only save on posts/pages if you have the correct rights. Otherwise just return the id without taking an action.
 		return $post_id;
 	}
 
 	// Return with no errors if the NONCE fails. This both blocks hack attacks AND prevents a save from removing
 	// the data if plugin modification adds security checks on meta box display that aren't duplicated here.
-	if (!wp_verify_nonce($_POST['pps_nonce'], plugin_basename(__FILE__))) return $post_id;
+	if ( !wp_verify_nonce( $_POST[ 'pps_nonce' ], plugin_basename( __FILE__ ) ) ) return $post_id;
 	
-	$replaced_sidebar = $_POST['tcc-pps-replace'];
-	$pps_active_on_post =  ($_POST['tcc-pps-active'] == 1) && (isset($wp_registered_sidebars[$replaced_sidebar]));
+	$replaced_sidebar = $_POST[ 'tcc-pps-replace' ];
+	$pps_active_on_post =  ( $_POST[ 'tcc-pps-active' ] == 1 ) && ( isset( $wp_registered_sidebars[ $replaced_sidebar ] ) );
 	
-	if ($pps_active_on_post) {
-		update_post_meta($post_id, PPS_KEY_REPLACED, $replaced_sidebar);
+	if ( $pps_active_on_post ) {
+		update_post_meta( $post_id, PPS_KEY_REPLACED, $replaced_sidebar );
 	} else {
-		delete_post_meta($post_id, PPS_KEY_REPLACED);
+		delete_post_meta( $post_id, PPS_KEY_REPLACED );
 	}
 	return $post_id;
 }
@@ -153,19 +158,19 @@ function pps_save_postdata($post_id) {
 // ************************************************************
 function pps_reg_custom_sidebar() {
 	global $wp_query, $wp_registered_sidebars;
-	if ( is_page() ) {
+	if ( is_page() || is_post() ) {
 		$post = $wp_query->get_queried_object();
 		$post_id = $post->ID;
 		$post_name = $post->post_name;
 		$post_title = $post->post_title;
-		$replaced_sidebar=get_post_meta($post->ID, PPS_KEY_REPLACED, true);
+		$replaced_sidebar=get_post_meta( $post->ID, PPS_KEY_REPLACED, true );
 
-		$pps_active_on_post = (isset($wp_registered_sidebars[$replaced_sidebar]) );
-		if ($pps_active_on_post) {
-			register_sidebar(array(
+		$pps_active_on_post = ( isset( $wp_registered_sidebars[ $replaced_sidebar ] ) );
+		if ( $pps_active_on_post ) {
+			register_sidebar( array( 
 				'name' => SIDEBAR_PREFIX . $post_name,
 				'id' => SIDEBAR_PREFIX . $post_name,
-				'description' => 'A custom sidebar for the post "' . $post_title . '"'));
+				'description' => 'A custom sidebar for the post "' . $post_title . '"' ) );
 		}
 	}
 }
@@ -179,12 +184,12 @@ function pps_reg_all_custom_sidebars() {
 	global $wp_registered_sidebars;
 	$posts = get_pps_post_ids();
 
-	foreach ($posts as $post_id) {
-		$post = get_post($post_id);
-		register_sidebar(array(
+	foreach ( $posts as $post_id ) {
+		$post = get_post( $post_id );
+		register_sidebar( array( 
 			'name' => SIDEBAR_PREFIX . $post->post_name,
 			'id' => SIDEBAR_PREFIX . $post->post_name,
-			'description' => 'A custom sidebar for the post "' . $post->post_title . '"'));
+			'description' => 'A custom sidebar for the post "' . $post->post_title . '"' ) );
 	}
 }
 
@@ -194,21 +199,29 @@ function pps_reg_all_custom_sidebars() {
 //  Replaces the content but not the design of sidebars during
 //  the page display.
 // ************************************************************
-function pps_hijack_sidebars($query) {
-	global $post, $wp_registered_sidebars, $_wp_sidebars_widgets;
-	$hostpost = $post;
+function pps_hijack_sidebars( $query ) {
+	global $post, $wp_registered_sidebars, $_wp_sidebars_widgets, $wp_query;
+	
+	if ( $wp_query->is_posts_page )	{
+		$hostpost = get_post( $wp_query->get_queried_object_id() );
+	} else {
+		$hostpost = $post;
+	}
+
 	$post_id = $hostpost->ID;	
 	$post_name = $hostpost->post_name;
-	$host_sidebar = get_post_meta($hostpost->ID, PPS_KEY_REPLACED, true);
-	while (($host_sidebar == '') and  ( $hostpost->post_parent > 0 )) {
-		$hostpost = get_post($hostpost->post_parent);
-		$host_sidebar = get_post_meta($hostpost->ID, PPS_KEY_REPLACED, true);
+	$host_sidebar = get_post_meta( $hostpost->ID, PPS_KEY_REPLACED, true );
+
+	while ( ( $host_sidebar == '' ) and  ( $hostpost->post_parent > 0 ) ) {
+		$hostpost = get_post( $hostpost->post_parent );
+		$host_sidebar = get_post_meta( $hostpost->ID, PPS_KEY_REPLACED, true );
 		$post_id = $hostpost->ID;	 
-		$post_name = $hostpost->post_name;
+		$post_name = $hostpost->post_name;		
 	} 
+	
 	$parasite_sidebar = SIDEBAR_PREFIX . $post_name;
-	if (isset($_wp_sidebars_widgets[$parasite_sidebar])) {
-		$_wp_sidebars_widgets[$host_sidebar] = $_wp_sidebars_widgets[$parasite_sidebar];
+	if ( isset( $_wp_sidebars_widgets[ $parasite_sidebar ] ) ) {
+		$_wp_sidebars_widgets[ $host_sidebar ] = $_wp_sidebars_widgets[ $parasite_sidebar ];
 	}
 }
 
@@ -216,15 +229,13 @@ function pps_hijack_sidebars($query) {
 // Executable Code
 // ************************************************************
 // Add the edit field to the Page Edit screens
-add_action('admin_menu', 'pps_add_custom_box'); 
+add_action( 'admin_menu', 'pps_add_custom_box' ); 
 
 // Process the custom PPS values submitted when saving pages.
-add_action('save_post', 'pps_save_postdata');
+add_action( 'save_post', 'pps_save_postdata' );
 
 // Register the custom sidebars that have been created for the pages.
-add_action('admin_init', 'pps_reg_all_custom_sidebars');
+add_action( 'admin_init', 'pps_reg_all_custom_sidebars' );
 
 // When a page is dipslayed, check for a custom sidebar and if it exists hijack the standard sidebar
 add_filter( 'wp', 'pps_hijack_sidebars' );	
-
-?>
--- readme.txt
+++ readme.txt
@@ -1,15 +1,15 @@
 === Per Page Sidebars ===
 Contributors: BrianLayman
 Tags: posts, pages, sidebars, widgets
-Donate: http://thecodecave.com/did
-Requires at least: 2.5
-Tested up to: 3.0.1
-Stable tag: 1.0
+Donate: http://thecodecave.com/donate
+Requires at least: 3.1
+Tested up to: 3.4
+Stable tag: 2.0.0
 
 The Per Page Sidebars (PPS) plugin allows blog administrators to create a unique sidebar for each Page. No template editing is required.
 
 == Description ==
-This plugin allows the creation and display of custom sidebars for any page on your site. On each page, you can choose which of your current theme's sidebars will be replaced. The replacement works for all descendants of a replaced page. 
+This plugin allows the creation and display of custom sidebars for any page (or post) on your site. On each page, you can choose which of your current theme's sidebars will be replaced. The replacement works for all descendants of a replaced page. 
 
 In this fashion you can:
 1. Easily create a CMS like site with unique sidebars for every page, even if your theme doesn't support it.
@@ -20,14 +20,26 @@ This plugin is compatible with every theme and all widgets because it hijacks yo
 
 == Installation ==
 
-1. Upload the `tcc-pps` directory to the `/wp-content/plugins/` directory.
-2. Activate the plugin through the 'Plugins' menu in WordPress.
-3. Modify the display settings as needed on the Settings > Per Page Sidebar page.
+The installation process.
+
+1. Upload  to the `/wp-content/plugins/` directory. Or directly upload from your Plugin management page.
+2. Activate the plugin through the 'Plugins' menu in WordPress
 
 == Screenshots ==
 
-1. custom_sidebar.png
-2. widget-box.png
+1. This is the metabox that appears on any page or post you are editing. It shows a list of all sidebars/widget areas built into the current theme. To replace one of them with a custom sidebar, you just need to mark the sidebar you are to replace.
+
+2. Under Appearance->Widgets, for each page or post on you've activated a custom sidebar, you will see a widget area like this one - named after the post or page on which it will be used.
+
+3. This plugin adds a "Custom Sidebar" metabox to the edit screens for pages and posts. Leaving the "Activate Custom Sidebar" box unchecked means that the plugin does not change the behavior of that page/post.
+
+4. This screenshot shows a typical blog running the 2010 theme and displaying the default sidebar.
+
+5. On the edit screen for any page or post, to create a custom sidebar, simply check "Activate Custom Sidebar" and choose the sidebar you will be replacing.
+
+6. When you activate a custom sidebar on a page or post, the Appearance->Widgets screen gets a widget area created for that page or post.
+
+7. Any widgets you put in the custom sidebar's widget area will be displayed with that page or post, completely replacing the chosen sidebar.
 
 == Use ==
 
@@ -36,6 +48,9 @@ This plugin is compatible with every theme and all widgets because it hijacks yo
 3. On the Appearance->Widgets screen, place the widgets on the newly created sidebar that matches your post. 
 
 That’s it. No code. No CSS changes. No custom templates.
+
+This video describes the usage of the plugin:
+ `[youtube http://www.youtube.com/watch?v=_U2XfrU7nZQ]`
 
 == Frequently Asked Questions ==
 
@@ -47,12 +62,19 @@ Some themes allow you to chose different sidebar layouts on each page.  Sometime
 Currently only one sidebar can be replaced per page.  If you wish to replace multiple sidebars per page, leave a commment and I may add this feature.
 
 = Can I replace sidebars on posts or custom post types? = 
-Currently only one pages can have their sidebars replaced.  If this is a disired feature, leave comments requesting it and I can add the feature fairly easily.
+YES! Version 2 of the plugin allows sidebars on posts.  However, custom post types are not yet supported.  If this is a disired feature, leave comments requesting it and I can add the feature fairly easily.
 
 = Can my authors & contributors replace the sidebar on a page? = 
 A user can only replace a sidebar on a page if their role has the *edit_theme_options* capability. A user can edit a page that has been assigned custom sidebar and that custom sidebar will not be dropped. A user without the *edit_theme_options* capability simply cannot turn that functionality on or off.
 
 == Changelog ==
 
-= 0.1 =
+= 1.0.0 =
+
 * Initial release.
+
+= 2.0.0 =
+
+* Added the ability to activate custom sidebars on posts
+* Fixed a bug that caused the wrong custom sidebar to be displayed on the page that was assigned as the Post page
+* Added screenshots and video to the documentation of the plugin
